ab770609227f686e94c4ff17337d5129b6e1a658
oleg <codemorgan@gmail.com>
2025-07-23 23:04:17 +0300

    Add PM2 config, improve server startup, and validate config
    Introduced a PM2 config sample for process management and logging. Improved the server initialization to handle missing or misconfigured YAML files gracefully. Updated dependencies, added Chalk for better logging, and changed the default server port to 3001.

--- service.js
+++ service.js
@@ -1,5 +1,4 @@
 import express from 'express';
-
 const app = express();
 import fs from 'fs'
 import {parse as YAMLParse} from 'yaml'
@@ -7,6 +6,19 @@ import resolvers from "./backend/resolvers.js";
 import bodyParser from "body-parser";
 import ApiError from "./backend/ApiError.js";
 import {getStepInstance} from "./backend/steps.js";
+import chalk from 'chalk';
+
+if(!fs.existsSync('./config.yml')) {
+    console.log(chalk.red("config.yml file not found"));
+    console.log(chalk.yellow("Please copy config.yml.sample to config.yml and fill it with your data."));
+    process.exit(1);
+}
+
+const config = YAMLParse(fs.readFileSync('./config.yml', 'utf8'));
+
+app.listen(config?.server.port, config?.server?.host,() => {
+    console.log(chalk.blue("Server started on "+chalk.green(config?.server?.host+":"+config?.server?.port)));
+});
 
 app.use(bodyParser.json({
     verify: (req, res, buf) => {
@@ -15,17 +27,16 @@ app.use(bodyParser.json({
     }
 }));
 
-const config = YAMLParse(fs.readFileSync('./config.yml', 'utf8'))
-
-const getRepository = (id) => config?.repositories[id];
+const getRepository = (id) => {
+    if (!config?.repositories[id])
+         throw new ApiError('Repository not found', 404);
+    return config?.repositories[id];
+}
 
 app.post("/deploy/:provider/:id", (req, res) => {
     try {
         const {provider, id} = req.params;
         const repo = getRepository(id);
-        if (!repo) {
-            throw new ApiError('Repository not found', 404);
-        }
         const resolver = resolvers[provider](req, repo);
         if (resolver.branch === repo.branch) {
             let stepResponses = [];
@@ -48,6 +59,3 @@ app.post("/deploy/:provider/:id", (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Service is running on port 3000");
-});

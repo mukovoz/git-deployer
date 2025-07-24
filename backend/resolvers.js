a222d0ae0df85b85ba4d75418ba3d3cd71c60ffc
oleg <codemorgan@gmail.com>
2025-07-24 11:46:57 +0300

    Gitlab support being added

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -1,4 +1,5 @@
 import crypto from 'crypto';
+
 /**
  * Most Git vendors use same way to sign their requests
  * @param request
@@ -12,7 +13,7 @@ const checkSignature = (request, secret) => {
 
     const hmac = crypto.createHmac('sha256', secret);
     const digest = `sha256=${hmac.update(request.rawBody).digest('hex')}`;
-    if(!crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(digest))){
+    if (!crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(digest))) {
         throw new Error('Invalid signature');
     }
     return true;
@@ -32,7 +33,16 @@ const BitBucketResolver = (request, repository) => {
     }
 
 }
-const GitLabResolver = () => {}
+const GitLabResolver = (
+    request, repository
+) => {
+    console.log(request.headers);
+    if (request.headers['x-gitlab-token'] !== repository?.secret)
+        throw new Error('Invalid token from GitLab');
+    return {
+        branch: request.json.ref.substring(11)
+    }
+}
 
 
 export default {
